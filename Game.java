600b27fe4dbb4662d89f0578a0391c6299fb30c6
polyp <bspaulsen@dons.usfca.edu>
2024-11-07 12:46:29 -0800

    Wrote basic GameRecord code

--- Game.java
+++ Game.java
@@ -1,6 +1,20 @@
 public abstract class Game {
     public static class GameRecord {
+        private Integer score;
+        private String playerID;
 
+        public GameRecord(Integer score, String playerID) {
+            this.score = score;
+            this.playerID = playerID;
+        }
+
+        public Integer getScore() {
+            return this.score;
+        }
+
+        public String getPlayerID() {
+            return this.playerID;
+        }
     }
     public static class AllGamesRecord {
     }
