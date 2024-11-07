c8d256694fda1062db31171c5146b2ed354d19fb
polyp <bspaulsen@dons.usfca.edu>
2024-11-07 12:50:42 -0800

    Made GameRecord implement comparable

--- Game.java
+++ Game.java
@@ -1,5 +1,5 @@
 public abstract class Game {
-    public static class GameRecord {
+    public static class GameRecord implements Comparable<GameRecord> {
         private Integer score;
         private String playerID;
 
@@ -14,6 +14,27 @@ public abstract class Game {
 
         public String getPlayerID() {
             return this.playerID;
+        }
+
+        @Override
+        public int compareTo(GameRecord otherGameRecord) {
+            return this.score - otherGameRecord.score;
+        }
+
+        @Override
+        public String toString() {
+            return playerID + " scored " + score;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if(other == null) {
+                return false;
+            }
+            if(!(other instanceof GameRecord)) {
+                return false;
+            }
+            return this.playerID.equals(((GameRecord) other).playerID) && this.score.equals(((GameRecord) other).score);
         }
     }
     public static class AllGamesRecord {
